49e288fdb0f56a48752a51256152738c97d8ee47
Luca Fenu <22529815+Knocks83@users.noreply.github.com>
2018-11-29 22:22:56 +0100

    Updated the way it checks for a result. Made a output array. Changed the position of where it creates the JSON object.

--- JSONHandling.php
+++ JSONHandling.php
@@ -1,36 +1,50 @@
 <?php
-    // You can find/create a API key here: https://saucenao.com/user.php?page=search-api
-    $api_key = 'Insert your API sauceNAO key here';
-    $image_url = 'Insert the image URL here';
-    
-    // Decode JSON data to PHP object
-    $decodedJSON = json_decode($json);
+// You can find/create a API key here: https://saucenao.com/user.php?page=search-api
+$api_key = 'Insert your SauceNAO API key here';
+$image_url = 'Insert the image URL here'; // The image MUSTN'T use HTTPS
 
-    // Creates various objects with the websites supported by SauceNAO (not all)    
-    $pixiv  = new \stdClass();      $pixiv->id = 5;         $pixiv->name = 'Pixiv';
-    $danbooru = new \stdClass();    $danbooru->id = 9;      $danbooru->name = 'Danbooru';
-    $yandere = new \stdClass();     $yandere->id = 12;      $yandere->name = 'Yande.re';
-    $animes = new \stdClass();      $animes->id = 21;       $animes->name = 'Anime';
-    $hAnime = new \stdClass();      $hAnime->id = 22;       $hAnime->name = 'Hentai';
-    $gelbooru = new \stdClass();    $gelbooru->id = 25;     $gelbooru->name = 'Gelbooru';
-    $konaChan = new \stdClass();    $konaChan->id = 26;     $konaChan->name = 'KonaChan';
-    $deviantArt = new \stdClass();  $deviantArt->id = 34;   $deviantArt->name = 'DeviantArt';
-    $manga = new \stdClass();       $manga->id = 36;        $manga->name = 'Manga';
+$json = file_get_contents("http://saucenao.com/search.php?db=5,9,12,21,22,25,26,34,36&api_key=$api_key&output_type=2&numres=6&url=$image_url");
 
-    // Creates an array with every website I want to check
-    $websites = [$pixiv,$danbooru,$yandere,$animes,$hAnime,$gelbooru,$konaChan,$deviantArt,$manga];
+// Decode JSON data to PHP object
+$json = json_decode($json);
 
-    $json = file_get_contents("https://saucenao.com/search.php?db=5,9,12,21,22,25,26,34,36&api_key=$api_key&output_type=2&numres=6&url=$image_url");
+// Creates various objects with the websites supported by SauceNAO (not all)
+$pixiv = new \stdClass();       $pixiv->id = 5;         $pixiv->name = 'Pixiv';
+$danbooru = new \stdClass();    $danbooru->id = 9;      $danbooru->name = 'Danbooru';
+$yandere = new \stdClass();     $yandere->id = 12;      $yandere->name = 'Yande.re';
+$animes = new \stdClass();      $animes->id = 21;       $animes->name = 'Anime';
+$hAnime = new \stdClass();      $hAnime->id = 22;       $hAnime->name = 'Hentai';
+$gelbooru = new \stdClass();    $gelbooru->id = 25;     $gelbooru->name = 'Gelbooru';
+$konaChan = new \stdClass();    $konaChan->id = 26;     $konaChan->name = 'KonaChan';
+$deviantArt = new \stdClass();  $deviantArt->id = 34;   $deviantArt->name = 'DeviantArt';
+$manga = new \stdClass();       $manga->id = 36;        $manga->name = 'Manga';
 
-    // Checks if the pic was found in the websites (by checking if the ID is in the header and doesn't have any error)
-    $found;
+// Creates an array with every website I want to check
+$websites = [$pixiv,$danbooru,$yandere,$animes,$hAnime,$gelbooru,$konaChan,$deviantArt,$manga];
 
-    foreach ($websites as $website) {
-        $id = $website->id;
-        if(property_exists($decodedJSON->header->index, $id)) {
-            if ($decodedJSON->header->index->$id->status == 0) {
+/*
+ Checks if the pic was found in the websites (by checking if the ID is in the header and doesn't have any error)
+ If it finds a website matching those characteristics it sets the property url to the website url
+*/
+
+if(property_exists($json->header, 'index')) {
+    $found = array();
+    $i = 0;
+    foreach ($json->header->index as $index) {
+        $i++;
+        foreach ($websites as $website) {
+            $id = $website->id;
+            if($index->id == $website->id && $index->status == 0) {
                 $website->exists = true;
+                $website->url = $json->results[$i]->data->ext_urls[0];
                 array_push($found, $website);
             }
         }
     }
+    unset($i);  // I delete the counter variable because it isn't needed anymore
+    
+    // Example processing of the obtained array
+    foreach ($found as $key => $value) {
+        echo("$value->name\t$value->url\n");
+    }
+} else die('Error: nothing found');
