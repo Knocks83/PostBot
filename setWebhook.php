06e1a4beca7b914fc97860aeb79c2a0903b206c0
Luca Fenu <22529815+Knocks83@users.noreply.github.com>
2018-12-24 19:06:09 +0100

    Fixed request

--- setWebhook.php
+++ setWebhook.php
@@ -1,11 +1,10 @@
 <?php
 require 'settings.php';
-$baseUrl = "https://api.telegram.org/bot$botToken/setwebhook?";
-$param = urlencode("url=$dirPath?fpam=$fpam%26token=$botToken");
+$url = "https://api.telegram.org/bot$botToken/setwebhook?url=$dirPath?fpam=$fpam%26token=$botToken";
 $ch = curl_init();
 
 $curlOptions = array(
-    CURLOPT_URL => $baseUrl.$param,
+    CURLOPT_URL => $url,
     CURLOPT_SSL_VERIFYPEER => false,
     CURLOPT_RETURNTRANSFER => true
 );
